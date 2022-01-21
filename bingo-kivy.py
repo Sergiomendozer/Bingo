63570f89deb4e686bf306298418a6d1f6679eda6
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-20 23:01:16 -0800

    kivy bingo notes

---
+++ bingo-kivy.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+import pygame
+from pygame.locals import *
+
+class StackLayout(StackLayout):
+    pass
+
+class GridLayout(GridLayout):
+    pass
+class Boxlayout(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class lab(App):
+    pass
+
+
+if __name__ == '__main__':
+    # game = Game()
+    # game.run()
+    Bingo.run()
