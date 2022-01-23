e30db3a1e6a945d561bf4e23d0a6c8133e1d51e5
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-22 17:59:58 -0800

    resize grid

--- Bingo.py
+++ Bingo.py
@@ -8,18 +8,18 @@ root.geometry("1015x600")
 
 ###### grab function from other file test out again
 #ROW 1
-Next_draw_Label = Label(root, text = "Next draw in: 5s", padx = 5, font =("Helvetica", 17)) # add time
-Random_number_picked_label = Label(root, text = "N-75", bg = "#1DED41", font =("Helvetica", 20), padx = 5)
-list_of_drawn_numbers_label = Label(root, text = "all random numbers picked list", bg = "#B900FF", font =("Helvetica", 15),padx = 5)
+Next_draw_Label = Label(root, text = "Next draw in: 5s", padx = 5, pady = 15, font =("Helvetica", 17)) # add time
+Random_number_picked_label = Label(root, text = "N-75", bg = "#1DED41", font =("Helvetica", 20), padx = 5,pady = 15, )
+list_of_drawn_numbers_label = Label(root, text = "all random numbers picked list", bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 15, )
 
 #ROW 1 positions
-Next_draw_Label.grid(row=1, column=1, columnspan = 2,sticky="nsew")
+Next_draw_Label.grid(row=1, column=1, columnspan = 2, sticky="nsew")
 Random_number_picked_label .grid(row=1, column=3,columnspan = 2, sticky="nsew")
-list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6,sticky="nsew")
+list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
 
 #ROW 2
-your_card_Label = Label(root, text = "Your card",bg = "#C91A3A", font =("Helvetica", 10), padx = 50)
-Bots_card_Label = Label(root, text = "Bots card", bg = "#1E9E23", font =("Helvetica", 10), padx = 50)
+your_card_Label = Label(root, text = "Your card",bg = "#FF4646", font =("Helvetica", 10), padx = 50)
+Bots_card_Label = Label(root, text = "Bots card", bg = "#96FF50", font =("Helvetica", 10), padx = 50)
 
 
 #ROW 2 positions
