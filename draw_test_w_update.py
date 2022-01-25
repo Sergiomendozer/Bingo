d99350aa5a47349231aabbdf84550a79ce776ada
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-24 18:49:30 -0800

    potential solution if not, then make sorted bingo list and just take [1] or 0 from list and take out

--- draw_test_w_update.py
+++ draw_test_w_update.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from drawing_numbers import random_bingo_number
+import time
 root = Tk()
 root.title("update Label")
 root.geometry("600x400")
@@ -9,7 +10,9 @@ def update(bingo_numbers):
     bingo_number = list(bingo_numbers)
     print(bingo_numbers)
     label_1.config(text= drawn_bingo_number,font =("Helvetica", 100), padx = 100, pady=100)
-    label_1.after(3000, update(bingo_numbers)) #list index out of range???
+    label_1.pack()
+    time.sleep(5)
+    #label_1.after(10000, update(bingo_numbers)) #list index out of range???
 
 
 label_1 = Label(root, text = "",font =("Helvetica", 100), padx = 100, pady=100)
