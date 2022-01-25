763fcedbfe65c6721a31d35fa0c79c64424e0ab0
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-25 02:59:09 -0800

    list is better as string add if len of string = then \nor delete str

--- Bingo.py
+++ Bingo.py
@@ -10,7 +10,6 @@ root = Tk()
 root.title("BINGO")
 root.geometry("1015x570")
 
-all_random_numbers_picked_list =[]
 # BINGO balls:
 ##B BINGO ball
 original_b_ball = Image.open("B-Bingo-ball.png") #call image b4 resize
@@ -47,26 +46,24 @@ resized_O = original_O_ball.resize((200,150), Image.ANTIALIAS) # resize(sides, h
 resized_O_ball = ImageTk.PhotoImage(resized_O)
 # bingo_ball=Label(root, image = resized_O_ball)
 
-def update_append_bingo_numbers():
-    list_of_drawn_numbers_label.config(text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
 
-list_of_drawn_numbers_label = Label(root, text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
-list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
 # drawing ball update label
-def update_drawn_ball(bingo_numbers):
+def update_drawn_ball(all_random_numbers_picked_list,bingo_numbers):
     (drawn_bingo_number,bingo_numbers) = random_bingo_number(bingo_numbers)
-    all_random_numbers_picked_list.append(drawn_bingo_number)
-    #call function to update all_random_numbers_picked_list
-    update_append_bingo_numbers()
-    # print(all_random_numbers_picked_list) ##### to delete
+    #call functions to update all_random_numbers_picked_list
+    all_random_numbers_picked_list = all_random_numbers_picked_list +" " + drawn_bingo_number
+    list_of_drawn_numbers_label.config(text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
+    # update_append_bingo_numbers(all_random_numbers_picked_list,drawn_bingo_number)
     Random_number_picked_label.config(text = drawn_bingo_number, font =("Helvetica", 24), bg = "#FFFFFF")
-    return Random_number_picked_label.after(5000,update_drawn_ball, bingo_numbers) #.after(parent, ms, function = None, *args)
-
-
+    return Random_number_picked_label.after(5000,update_drawn_ball,all_random_numbers_picked_list, bingo_numbers) #.after(parent, ms, function = None, *args)
+    
+list_of_drawn_numbers_label = Label(root, text = "", bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
+list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
 Random_number_picked_label =Label(root,text = " fff  ", font =("Helvetica", 24), bg = "#FFFFFF") 
 Random_number_picked_label.place(x = 260, y =55) 
 bingo_numbers = ["B 1","B 2","B 3","B 4","B 4","B 5","B 7", "B 8","B 9","B 10","B 11","B 12","B 13","B 14","B 15","I 16","I 17","I 18","I 19","I 20","I 21","I 22","I 23","I 24","I 25","I 26","I 27","I 28","I 29","I 30","N 31","N 32","N 33","N 34","N 35","N 36","N 37","N 38","N 39","N 40","N 41","N 42","N 42","N 44","N 45","G 46","G 47","G 48","G 49","G 50","G 51","G 52","G 53","G 54","G 55","G 56","G 57","G 58","G 59","G 60","O 61","O 62","O 63","O 64","O 65","O 66","O 67","O 68","O 69","O 70","O 71","O 72","O 73","O 74","O 75"]
-update_drawn_ball(bingo_numbers)
+all_random_numbers_picked_list =""
+update_drawn_ball(all_random_numbers_picked_list,bingo_numbers)
 
 ### timer for next ball
 def update_timer_countdown(n):
