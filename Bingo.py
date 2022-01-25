f7472c5e5dd696c6959d9817d9cfa7634424a1c4
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-25 04:41:45 -0800

    Bingo ball colors

--- Bingo.py
+++ Bingo.py
@@ -12,11 +12,11 @@ root.geometry("1015x570")
 
 # BINGO balls:
 ##B BINGO ball
-original_b_ball = Image.open("B-Bingo-ball.png") #call image b4 resize
+original_B_ball = Image.open("B-Bingo-ball.png") #call image b4 resize
 #resizing image
-resized_b = original_b_ball.resize((200,150), Image.ANTIALIAS) # resize(sides, height)
-resized_b_ball = ImageTk.PhotoImage(resized_b)
-bingo_ball =Label(root, image = resized_b_ball)
+resized_B = original_B_ball.resize((200,150), Image.ANTIALIAS) # resize(sides, height)
+resized_B_ball = ImageTk.PhotoImage(resized_B)
+# bingo_ball =Label(root, image = resized_b_ball)
 
 ## I BINGO ball
 original_I_ball = Image.open("I-Bingo-ball.png") #call image b4 resize
@@ -46,6 +46,25 @@ resized_O = original_O_ball.resize((200,150), Image.ANTIALIAS) # resize(sides, h
 resized_O_ball = ImageTk.PhotoImage(resized_O)
 # bingo_ball=Label(root, image = resized_O_ball)
 
+bingo_ball =Label(root, image = resized_O_ball)
+def bingo_ball_color(drawn_bingo_number):
+    if drawn_bingo_number[0] == "B":
+        print ("B")
+        bingo_ball.config(image = resized_B_ball)
+    elif drawn_bingo_number[0] == "I":
+        print ("I")
+        bingo_ball.config(image = resized_I_ball)
+    elif drawn_bingo_number[0] == "N":
+        print ("N")
+        bingo_ball.config(image = resized_N_ball)
+    elif drawn_bingo_number[0] == "G":
+        print ("G")
+        bingo_ball.config(image = resized_G_ball)
+    elif drawn_bingo_number[0] == "O":
+        print ("O")
+        bingo_ball.config(image = resized_O_ball)
+
+    # .config(
 
 # drawing ball update label
 def update_drawn_ball(all_random_numbers_picked_list,bingo_numbers):
@@ -54,25 +73,28 @@ def update_drawn_ball(all_random_numbers_picked_list,bingo_numbers):
         all_random_numbers_picked_list = all_random_numbers_picked_list +" \n" + drawn_bingo_number
         list_of_drawn_numbers_label.config(text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
         Random_number_picked_label.config(text = drawn_bingo_number, font =("Helvetica", 24), bg = "#FFFFFF")
+        bingo_ball_color(drawn_bingo_number)
         return Random_number_picked_label.after(5000,update_drawn_ball,all_random_numbers_picked_list, bingo_numbers) #.after(parent, ms, function = None, *args)
     elif len(all_random_numbers_picked_list) == 104 or len(all_random_numbers_picked_list) == 105 or len(all_random_numbers_picked_list) == 106 or len(all_random_numbers_picked_list) == 107:
         (drawn_bingo_number,bingo_numbers) = random_bingo_number(bingo_numbers)
         all_random_numbers_picked_list = all_random_numbers_picked_list +" \n" + drawn_bingo_number
         list_of_drawn_numbers_label.config(text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
         Random_number_picked_label.config(text = drawn_bingo_number, font =("Helvetica", 24), bg = "#FFFFFF")
+        bingo_ball_color(drawn_bingo_number)
         return Random_number_picked_label.after(5000,update_drawn_ball,all_random_numbers_picked_list, bingo_numbers) #.after(parent, ms, function = None, *args)
     else:
         (drawn_bingo_number,bingo_numbers) = random_bingo_number(bingo_numbers)
         all_random_numbers_picked_list = all_random_numbers_picked_list +" " + drawn_bingo_number
         list_of_drawn_numbers_label.config(text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
         Random_number_picked_label.config(text = drawn_bingo_number, font =("Helvetica", 24), bg = "#FFFFFF")
+        bingo_ball_color(drawn_bingo_number)
         return Random_number_picked_label.after(5000,update_drawn_ball,all_random_numbers_picked_list, bingo_numbers) #.after(parent, ms, function = None, *args)
 
 
 list_of_drawn_numbers_label = Label(root, text = "", bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
 list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
 Random_number_picked_label =Label(root,text = " fff  ", font =("Helvetica", 24), bg = "#FFFFFF") 
-Random_number_picked_label.place(x = 260, y =53) 
+Random_number_picked_label.place(x = 259, y =53) 
 bingo_numbers = ["B 1","B 2","B 3","B 4","B 4","B 5","B 7", "B 8","B 9","B 10","B 11","B 12","B 13","B 14","B 15","I 16","I 17","I 18","I 19","I 20","I 21","I 22","I 23","I 24","I 25","I 26","I 27","I 28","I 29","I 30","N 31","N 32","N 33","N 34","N 35","N 36","N 37","N 38","N 39","N 40","N 41","N 42","N 42","N 44","N 45","G 46","G 47","G 48","G 49","G 50","G 51","G 52","G 53","G 54","G 55","G 56","G 57","G 58","G 59","G 60","O 61","O 62","O 63","O 64","O 65","O 66","O 67","O 68","O 69","O 70","O 71","O 72","O 73","O 74","O 75"]
 all_random_numbers_picked_list =""
 update_drawn_ball(all_random_numbers_picked_list,bingo_numbers)
@@ -94,9 +116,6 @@ timer.grid(row=1, column=1, columnspan = 2, sticky="nsew")
 update_timer_countdown(6)
 
 #ROW 1
-# list_of_drawn_numbers_label = Label(root, text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
-# list_of_drawn_numbers_label = Label(root, text = all_random_numbers_picked_list, bg = "#B900FF", font =("Helvetica", 15),padx = 5, pady = 45, )
-# list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
 #ROW 1 positions
 bingo_ball.grid(row=1, column=3,columnspan = 2, sticky="nsew") # chaneg to bingo_ball
 # list_of_drawn_numbers_label.grid(row=1, column=5, columnspan = 6, sticky="nsew")
