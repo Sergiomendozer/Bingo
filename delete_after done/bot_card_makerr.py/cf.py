8e92cfea47116e725eda24cf62183d120774b876
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-28 05:03:25 -0800

    deleted lots of old files

--- delete_after done/bot_card_makerr.py/cf.py
+++
@@ -1,8 +0,0 @@
-# def bot_draw_B_numbers_row_5(bot_numbers_for_B_list_out_4):
-#     bot_row_5_B_number = random.choice(bot_numbers_for_B_list_out_4)
-#     # takes out drawing number from the list
-#     bot_take_out_of_list_B = bot_numbers_for_B_list_out_4.index(bot_row_5_B_number)
-#     bot_numbers_for_B_list_out_4.pop(bot_take_out_of_list_B)
-#     bot_numbers_for_B_list_out_5 = bot_numbers_for_B_list_out_4
-#     print(bot_row_5_B_number, bot_numbers_for_B_list_out_5)  ### delete
-#     return bot_row_5_B_number, bot_numbers_for_B_list_out_5
--- delete_after done/clock_example.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from drawing_numbers import random_bingo_number
-import time
-root = Tk()
-root.title("update Label")
-root.geometry("600x400")
-
-def clock():
-    return ("none")
