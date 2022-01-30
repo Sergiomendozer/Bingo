256672e81f7147e4a5f980b620acbfdede79597a
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-30 00:14:39 -0800

    calls for player card function made

--- Bingo.py
+++ Bingo.py
@@ -1020,7 +1020,6 @@ row_5_O_number = ""
 # )
 # row_5_B_number = draw_B_numbers_row_5(numbers_for_B_list_out_4)
 
-###mark
 
 # player card maker for I row:
 # def draw_I_numbers_row_5(numbers_for_I_list_out_4):
@@ -1685,7 +1684,69 @@ def draw_B_numbers_row_1():
     return draw_B_numbers_row_2(numbers_for_B_list_out_1)
 
 
+# player card maker for I row:
+# def draw_I_numbers_row_5(numbers_for_I_list_out_4):
+#     row_5_I_number = random.choice(numbers_for_I_list_out_4)
+#     # takes out drawing number from the list
+#     take_out_of_list_I = numbers_for_I_list_out_4.index(row_5_I_number)
+#     numbers_for_I_list_out_4.pop(take_out_of_list_I)
+#     return row_5_I_number
+
+
+# def draw_I_numbers_row_4(numbers_for_I_list_out_3):
+#     row_4_I_number = random.choice(numbers_for_I_list_out_3)
+#     # takes out drawing number from the list
+#     take_out_of_list_I = numbers_for_I_list_out_1.index(row_4_I_number)
+#     numbers_for_I_list_out_3.pop(take_out_of_list_I)
+#     numbers_for_I_list_out_4 = numbers_for_I_list_out_3
+#     return row_4_I_number, numbers_for_I_list_out_3
+
+
+# def draw_I_numbers_row_3(numbers_for_I_list_out_2):
+#     row_3_I_number = random.choice(numbers_for_I_list_out_2)
+#     # takes out drawing number from the list
+#     take_out_of_list_I = numbers_for_I_list_out_2.index(row_3_I_number)
+#     numbers_for_I_list_out_2.pop(take_out_of_list_I)
+#     numbers_for_I_list_out_3 = numbers_for_I_list_out_2
+#     return row_3_I_number, numbers_for_I_list_out_3
+
+
+# def draw_I_numbers_row_2(numbers_for_I_list_out_1):
+#     row_2_I_number = random.choice(numbers_for_I_list_out_1)
+#     # takes out drawing number from the list
+#     take_out_of_list_I = numbers_for_I_list_out_1.index(row_2_I_number)
+#     numbers_for_I_list_out_1.pop(take_out_of_list_I)
+#     numbers_for_I_list_out_2 = numbers_for_I_list_out_1
+#     return row_2_I_number, numbers_for_I_list_out_2
+
+
+# def draw_I_numbers_row_1():
+#     numbers_for_I_list = []
+#     numbers_for_I_list_int = [i for i in range(16, 31)]
+#     for e in numbers_for_I_list_int:
+#         e = str(e)
+#         numbers_for_I_list.append(e)
+#     row_1_I_number = random.choice(numbers_for_I_list)
+#     # takes out drawing number from the list
+#     take_out_of_list_I = numbers_for_I_list.index(row_1_I_number)
+#     numbers_for_I_list.pop(take_out_of_list_I)
+#     numbers_for_I_list_out_1 = numbers_for_I_list
+#     return row_1_I_number, numbers_for_I_list_out_1
+
+# player card maker for N row:
+
+# player card maker for G row:
+
+# player card maker for O row:
+
+###MARK
+# Call functions that make player's card
 draw_B_numbers_row_1()
+# draw_I_numbers_row_1()
+# draw_N_numbers_row_1()
+# draw_G_numbers_row_1()
+# draw_O_numbers_row_1()
+
 # function for BINGO_button:
 def reset_bingo_button():  # resets to original color
     Bingo_button.config(
@@ -1698,11 +1759,12 @@ def click_BINGO():
     return Bingo_button.after(3000, reset_bingo_button)
 
 
-### NEW
+###NEW
 def make_a_new_player_card():
     # game needs to be paused
     New_card.config(text="NEW called", bg="#FF0000", font=("Helvetica", 20))
     # calls function for row B maker
+    draw_B_numbers_row_1()
     # row_1_B_number, numbers_for_B_list_out_1 = draw_B_numbers_row_1()
     # row_2_B_number, numbers_for_B_list_out_2 = draw_B_numbers_row_2(
     #     numbers_for_B_list_out_1
@@ -1714,8 +1776,8 @@ def make_a_new_player_card():
     #     numbers_for_B_list_out_3
     # )
     # row_5_B_number = draw_B_numbers_row_5(numbers_for_B_list_out_4)
-    # # update .configure
-    # # .config( ### here
+    # update .configure
+    # .config( ### here
     # B_bingo_row_1.configure(
     #     text=row_1_B_number,
     #     bg=click_row_1_B.colors[0],  # 1st col to start
