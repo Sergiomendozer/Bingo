b49138ff7ba58f9e2b02712eccb30c67e11bb25c
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-01 03:12:25 -0800

    corrected test and bingo.find()

--- find_bingo_tests.py
+++ find_bingo_tests.py
@@ -8,25 +8,42 @@ row_3_B_number = 3
 row_4_B_number = 4
 row_5_B_number = 5
 row_1_B_number = str(row_1_B_number)
-row_2_B_number = str(row_1_B_number)
-row_3_B_number = str(row_1_B_number)
-row_4_B_number = str(row_1_B_number)
-row_5_B_number = str(row_1_B_number)
+row_2_B_number = str(row_2_B_number)
+row_3_B_number = str(row_3_B_number)
+row_4_B_number = str(row_4_B_number)
+row_5_B_number = str(row_5_B_number)
 
 # I column test:
 I_list_drawn_str = "16 17 18 19 20 21 22 23 24 25 26 27 28 29 30"
+# I_list_drawn_str = ""
 row_1_I_number = 16
 row_2_I_number = 18
 row_3_I_number = 23
 row_4_I_number = 24
 row_5_I_number = 25
 row_1_I_number = str(row_1_I_number)
-row_2_I_number = str(row_1_I_number)
-row_3_I_number = str(row_1_I_number)
-row_4_I_number = str(row_1_I_number)
-row_5_I_number = str(row_1_I_number)
+row_2_I_number = str(row_2_I_number)
+row_3_I_number = str(row_3_I_number)
+row_4_I_number = str(row_4_I_number)
+row_5_I_number = str(row_5_I_number)
 
-# column B check if bingo
+# column N check if bingo
+# N_list_drawn_str = "16 17 18 19 20 21 22 23 24 25 26 27 28 29 30"
+# #I_list_drawn_str = ""
+# row_1_N_number = 16
+# row_2_N_number = 18
+# row_3_N_number = 23
+# row_4_N_number = 24
+# row_5_N_number = 25
+# row_1_N_number = str(row_1_N_number)
+# row_2_N_number = str(row_1_N_number)
+# row_3_N_number = str(row_1_N_number)
+# row_4_N_number = str(row_1_N_number)
+# row_5_N_number = str(row_1_N_number)
+
+# column G check if bingo
+# column G check if bingo
+
 if (
     B_list_drawn_str.find(row_1_B_number) != -1
     and B_list_drawn_str.find(row_2_B_number) != -1
