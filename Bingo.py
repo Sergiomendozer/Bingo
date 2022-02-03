2e564246bbb369efdb8f5e9fe2a7a8ac94ac2587
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-03 01:09:44 -0800

    deleted old code that is commented

--- Bingo.py
+++ Bingo.py
@@ -719,69 +719,6 @@ def click_row_5_O():
 click_row_5_O.click = 0
 click_row_5_O.colors = ["#00FF33", "#B900ff"]
 
-# # Bot strings for cards empty
-# bot_row_1_B_number = ""
-# bot_row_2_B_number = ""
-# bot_row_3_B_number = ""
-# bot_row_4_B_number = ""
-# bot_row_5_B_number = ""
-
-# bot_row_1_I_number = ""
-# bot_row_2_I_number = ""
-# bot_row_3_I_number = ""
-# bot_row_4_I_number = ""
-# bot_row_5_I_number = ""
-
-# bot_row_1_N_number = ""
-# bot_row_2_N_number = ""
-# bot_row_3_N_number = ""
-# bot_row_4_N_number = ""
-# bot_row_5_N_number = ""
-
-# bot_row_1_G_number = ""
-# bot_row_2_G_number = ""
-# bot_row_3_G_number = ""
-# bot_row_4_G_number = ""
-# bot_row_5_G_number = ""
-
-# bot_row_1_O_number = ""
-# bot_row_2_O_number = ""
-# bot_row_3_O_number = ""
-# bot_row_4_O_number = ""
-# bot_row_5_O_number = ""
-
-# # player strings for cards empty
-# row_1_B_number = ""
-# row_2_B_number = ""
-# row_3_B_number = ""
-# row_4_B_number = ""
-# row_5_B_number = ""
-
-# row_1_I_number = ""
-# row_2_I_number = ""
-# row_3_I_number = ""
-# row_4_I_number = ""
-# row_5_I_number = ""
-
-# row_1_N_number = ""
-# row_2_N_number = ""
-# row_3_N_number = ""
-# row_4_N_number = ""
-# row_5_N_number = ""
-
-# row_1_G_number = ""
-# row_2_G_number = ""
-# row_3_G_number = ""
-# row_4_G_number = ""
-# row_5_G_number = ""
-
-# row_1_O_number = ""
-# row_2_O_number = ""
-# row_3_O_number = ""
-# row_4_O_number = ""
-# row_5_O_number = ""
-
-
 # BINGO row 1
 B_bingo_row_1 = Button(
     root,
