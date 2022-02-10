f4f2547bdf149aa1276266c5ecb03708a84e9e7b
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-10 00:13:23 -0800

    G_list_drawn_str made global

--- Bingo.py
+++ Bingo.py
@@ -141,9 +141,8 @@ def sort_string(string):
 # * highlighted bookmark
 def bingo_ball_color(
     drawn_bingo_number,
-    O_list_drawn_str,
 ):
-    global B_list_drawn_str, I_list_drawn_str, N_list_drawn_str, G_list_drawn_str
+    global B_list_drawn_str, I_list_drawn_str, N_list_drawn_str, G_list_drawn_str, O_list_drawn_str
     if drawn_bingo_number.startswith("B"):
         B_list_drawn_list = []
         bingo_ball.config(image=resized_B_ball)
@@ -227,6 +226,7 @@ def bingo_ball_color(
         bingo_ball.config(image=resized_O_ball)
         O_list_drawn_str = O_list_drawn_str + " " + drawn_bingo_number[2:]
         O_list_drawn_str = sort_string(O_list_drawn_str)
+        O_list_drawn_str = O_list_drawn_str + " "
         O_list_drawn.config(
             text="O: " + O_list_drawn_str,
             bg="#00FF33",
@@ -270,7 +270,6 @@ def update_drawn_ball(
             O_list_drawn_str,
         ) = bingo_ball_color(
             drawn_bingo_number,
-            O_list_drawn_str,
         )
         return Random_number_picked_label.after(
             5000,
