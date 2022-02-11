a2abb13dc9db182e9908452be13d5280d4ef1537
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-11 01:05:27 -0800

    function in process

---
+++ test.py
@@ -0,0 +1,18 @@
+# B_list_drawn_str = "1 2 3 4 5"
+# # converts str to a list
+# B_list_drawn_str = B_list_drawn_str.split()
+
+# #converts list back into list
+# print(B_list_drawn_str)
+
+
+## take current drawn number
+
+### new
+drawn_bingo_number = 1
+bot_row_1_G_number = 1
+# bot_row_1_G_number = 2
+if bot_row_1_G_number == drawn_bingo_number:
+    print("pass")
+else:
+    print("else")
--- was_number_called_for_bot.py
+++ was_number_called_for_bot.py
@@ -1,39 +1,56 @@
-bot_row_1_B_number = ""
-bot_row_2_B_number = ""
-bot_row_3_B_number = ""
-bot_row_4_B_number = ""
-bot_row_5_B_number = ""
+# bot_row_1_B_number = ""
+# bot_row_2_B_number = ""
+# bot_row_3_B_number = ""
+# bot_row_4_B_number = ""
+# bot_row_5_B_number = ""
 
-bot_row_1_I_number = ""
-bot_row_2_I_number = ""
-bot_row_3_I_number = ""
-bot_row_4_I_number = ""
-bot_row_5_I_number = ""
+# bot_row_1_I_number = ""
+# bot_row_2_I_number = ""
+# bot_row_3_I_number = ""
+# bot_row_4_I_number = ""
+# bot_row_5_I_number = ""
 
-bot_row_1_N_number = ""
-bot_row_2_N_number = ""
-bot_row_3_N_number = ""
-bot_row_4_N_number = ""
-bot_row_5_N_number = ""
+# bot_row_1_N_number = ""
+# bot_row_2_N_number = ""
+# bot_row_3_N_number = ""
+# bot_row_4_N_number = ""
+# bot_row_5_N_number = ""
 
-bot_row_1_G_number = ""
-bot_row_2_G_number = ""
-bot_row_3_G_number = ""
-bot_row_4_G_number = ""
-bot_row_5_G_number = ""
+# bot_row_1_G_number = ""
+# bot_row_2_G_number = ""
+# bot_row_3_G_number = ""
+# bot_row_4_G_number = ""
+# bot_row_5_G_number = ""
 
-bot_row_1_O_number = ""
-bot_row_2_O_number = ""
-bot_row_3_O_number = ""
-bot_row_4_O_number = ""
-bot_row_5_O_number = ""
+# bot_row_1_O_number = ""
+# bot_row_2_O_number = ""
+# bot_row_3_O_number = ""
+# bot_row_4_O_number = ""
+# bot_row_5_O_number = ""
 
-### above is just for test
+# B_list_drawn_str = " "
+# I_list_drawn_str = " "
+# N_list_drawn_str = " "
+# G_list_drawn_str = " "
+# O_list_drawn_str = " "
+
+# drawn_bingo_number = 1
+
+# B_bingo_row_1_bot = Label(
+#     root, text=bot_row_1_B_number, bg="#00CCFF", font=("Helvetica", 20)
+# )
+# ### above is just for test
 
 
-def was_number_called_for_bot():
-    global bot_row_1_B_number, bot_row_2_B_number, bot_row_3_B_number, bot_row_4_B_number, bot_row_5_B_number, bot_row_1_I_number, bot_row_2_I_number, bot_row_3_I_number, bot_row_4_I_number, bot_row_5_I_number, bot_row_1_N_number, bot_row_2_N_number, bot_row_3_N_number, bot_row_4_N_number, bot_row_5_N_number, bot_row_1_G_number, bot_row_2_G_number, bot_row_3_G_number, bot_row_4_G_number, bot_row_5_G_number, bot_row_1_O_number, bot_row_2_O_number, bot_row_3_O_number, bot_row_4_O_number, bot_row_5_O_number
-
-
-if bot_row_1_B_number == "":
-    pass
+# def was_number_called_for_bot():
+#     global drawn_bingo_number, bot_row_1_B_number, bot_row_2_B_number, bot_row_3_B_number, bot_row_4_B_number, bot_row_5_B_number, bot_row_1_I_number, bot_row_2_I_number, bot_row_3_I_number, bot_row_4_I_number, bot_row_5_I_number, bot_row_1_N_number, bot_row_2_N_number, bot_row_3_N_number, bot_row_4_N_number, bot_row_5_N_number, bot_row_1_G_number, bot_row_2_G_number, bot_row_3_G_number, bot_row_4_G_number, bot_row_5_G_number, bot_row_1_O_number, bot_row_2_O_number, bot_row_3_O_number, bot_row_4_O_number, bot_row_5_O_number
+# if bot_row_1_B_number == drawn_bingo_number:
+#     B_bingo_row_1_bot.config(text=bot_row_1_B_number, bg="#B900FF", font=("Helvetica", 20))
+# elif bot_row_2_B_number == drawn_bingo_number:
+#     B_bingo_row_2_bot.config(text=bot_row_2_B_number, bg="#B900FF", font=("Helvetica", 20))
+# elif bot_row_3_B_number == drawn_bingo_number:
+#     B_bingo_row_3_bot.config(text=bot_row_3_B_number, bg="#B900FF", font=("Helvetica", 20))
+# elif bot_row_4_B_number == drawn_bingo_number:
+#     B_bingo_row_4_bot.config(text=bot_row_4_B_number, bg="#B900FF", font=("Helvetica", 20))
+# elif bot_row_5_B_number == drawn_bingo_number:
+#     B_bingo_row_5_bot.config(text=bot_row_5_B_number, bg="#B900FF", font=("Helvetica", 20))
