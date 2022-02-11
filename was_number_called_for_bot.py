52bd0fe6dde3fb65c3fdea01d098a184bb7eb2a6
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-11 00:20:46 -0800

    frame work for chnaging bots card color

---
+++ was_number_called_for_bot.py
@@ -0,0 +1,39 @@
+bot_row_1_B_number = ""
+bot_row_2_B_number = ""
+bot_row_3_B_number = ""
+bot_row_4_B_number = ""
+bot_row_5_B_number = ""
+
+bot_row_1_I_number = ""
+bot_row_2_I_number = ""
+bot_row_3_I_number = ""
+bot_row_4_I_number = ""
+bot_row_5_I_number = ""
+
+bot_row_1_N_number = ""
+bot_row_2_N_number = ""
+bot_row_3_N_number = ""
+bot_row_4_N_number = ""
+bot_row_5_N_number = ""
+
+bot_row_1_G_number = ""
+bot_row_2_G_number = ""
+bot_row_3_G_number = ""
+bot_row_4_G_number = ""
+bot_row_5_G_number = ""
+
+bot_row_1_O_number = ""
+bot_row_2_O_number = ""
+bot_row_3_O_number = ""
+bot_row_4_O_number = ""
+bot_row_5_O_number = ""
+
+### above is just for test
+
+
+def was_number_called_for_bot():
+    global bot_row_1_B_number, bot_row_2_B_number, bot_row_3_B_number, bot_row_4_B_number, bot_row_5_B_number, bot_row_1_I_number, bot_row_2_I_number, bot_row_3_I_number, bot_row_4_I_number, bot_row_5_I_number, bot_row_1_N_number, bot_row_2_N_number, bot_row_3_N_number, bot_row_4_N_number, bot_row_5_N_number, bot_row_1_G_number, bot_row_2_G_number, bot_row_3_G_number, bot_row_4_G_number, bot_row_5_G_number, bot_row_1_O_number, bot_row_2_O_number, bot_row_3_O_number, bot_row_4_O_number, bot_row_5_O_number
+
+
+if bot_row_1_B_number == "":
+    pass
